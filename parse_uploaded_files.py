9ca361a42087e80436fde0567dc590b14f393476
Aravind Mantravadi <aravind1338@gmail.com>
2020-07-14 12:40:07 -0400

    Storing email address in text file

--- parse_uploaded_files.py
+++ parse_uploaded_files.py
@@ -23,9 +23,9 @@ def main():
 
     # Share the email address among other gboxes using a pickle dump #
     email_address = gn.get_arg("email_address")
-    shared = {"email_address":email_address}
-    with open(Path("shared.pickle"), "wb") as fp:
-        pickle.dump(shared, fp)
+    #shared = {"email_address":email_address}
+    with open(Path("shared.txt"), "w") as f:
+        f.write(email_address)
 
     if file_format == "csv":
         tb = pd.read_csv(assay_file, sep=",", index_col=0, engine='c', memory_map=True)
